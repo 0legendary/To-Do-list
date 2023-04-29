9d63a797d063184d39acceb388d77f1ee503dbd0
Alen.m <alenmullassery123@gmail.com>
2023-04-29 12:03:42 +0530

    developed the base of ToDo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [toDos, setToDos] = useState([])
-  const [toDo, setTodo] = useState([''])
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setTodo] = useState('');
+  const [scheduled, setScheduled] = useState([]);
+
   return (
     <div className="app">
       <div className='to-do-list'>
@@ -24,9 +25,6 @@ function App() {
       </div>
       <div class="lists-container">
 
-
-
-
         {/* Finished list */}
         <section class="finished-list">
           <div class="container">
@@ -42,8 +40,6 @@ function App() {
           </div>
         </section>
 
-
-
         {/* Progress list */}
         <section class="progress-list">
           <div class="container">
@@ -54,60 +50,55 @@ function App() {
                 {
                   toDos.map((obj) => {
 
-
-
                     return (
-
                       <div className="todo">
                         <div className="left">
-                          <input onChange={(e) => {
-                            console.log(e.target.checked)
-                            console.log(obj)
-                            setToDos(toDos.filter(obj2 => {
-                              if (obj2.id === obj.id) {
-                                obj2.status = e.target.checked
-                              }
-                              return obj2
-                            }))
-
-                          }} value={obj.status} type="checkbox" name="" id="" />
+                          <input
+                            onChange={(e) => {
+                              setToDos(toDos.map((obj2) => {
+                                if (obj2.id === obj.id) {
+                                  obj2.status = e.target.checked
+                                }
+                                return obj2
+                              }));
+                            }}
+                            value={obj.status}
+                            type="checkbox"
+                            name=""
+                            id=""
+                          />
                           <p>{obj.text}</p>
                         </div>
                         <div className="right">
-                          <i onClick className="fas fa-times"></i>
-
+                          <i onClick={() => {
+                            setToDos(toDos.filter(obj2 => obj2.id !== obj.id));
+                            setScheduled([...scheduled, { id: Date.now(), text: obj.text, status: false }]);
+                          }} className="fas fa-times"></i>
                         </div>
                       </div>
                     )
-
-
                   })
                 }
               </div>
 
-
             </div>
           </div>
         </section>
-
-
 
         {/* Scheduled list */}
         <section class="scheduled-list">
           <div class="container">
             <div class="scheduled-list-border">
               <h1>Scheduled List</h1>
-
+              {scheduled.map((obj) => {
+                return (<h1>{obj.text}</h1>)
+              })}
             </div>
           </div>
         </section>
 
-
-
       </div>
     </div>
-
-
   );
 }
 
